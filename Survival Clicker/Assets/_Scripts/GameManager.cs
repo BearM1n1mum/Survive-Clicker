05e0d3d6a1ee2aeb14e6544d05858e7fc04fd8c3
BearM1n1mum <bob.gaming.cro@gmail.com>
2025-07-04 01:25:53 +0200

    Survive Clicker UI setup

---
+++ Survival Clicker/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,145 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject titleScreenPanel;
+    [SerializeField] private GameObject mainMenuScreenPanel;
+    [SerializeField] private GameObject optionsScreenPanel;
+    [SerializeField] private GameObject pauseMenuScreenPanel;
+    [SerializeField] private GameObject endScreenPanel;
+
+
+    [Header("Buttons & sliders")]
+    [SerializeField] private Slider volume;
+    [SerializeField] private TMP_Text volumeValue;
+    [SerializeField] private GameObject pressAnyKey;
+    [SerializeField] private GameObject optionsKey;
+    [SerializeField] private GameObject backToMainMenu;
+
+    private bool hasPressedAnyKey;
+
+    private void Awake()
+    {
+        TitleScreen();
+    }
+
+   
+
+    private void Update()
+    {
+        if (!hasPressedAnyKey && Input.anyKeyDown)
+        {
+            hasPressedAnyKey = true;
+            Debug.Log($"A mouse click or a key press was detected Update");
+            TitleScreen();
+            ShowMainMenu();
+        }
+            VolumeValue();
+        if (Input.GetKeyDown(KeyCode.Escape) && !mainMenuScreenPanel.activeInHierarchy && !endScreenPanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+    }
+
+    public void TitleScreen()
+    {
+        
+
+        titleScreenPanel.SetActive(true);
+
+        mainMenuScreenPanel.SetActive(false);
+
+        optionsScreenPanel.SetActive(false);
+
+        pauseMenuScreenPanel.SetActive(false);
+
+        endScreenPanel.SetActive(false);
+
+         
+    }
+
+    public void ShowMainMenu()
+    {
+        
+        titleScreenPanel.SetActive(false);
+
+        mainMenuScreenPanel.SetActive(true);
+
+        optionsScreenPanel.SetActive(false);
+
+        pauseMenuScreenPanel.SetActive(false);
+
+        endScreenPanel.SetActive(false);
+    }
+    public void ShowOptionsMenu()
+    {
+        titleScreenPanel.SetActive(false);
+
+        mainMenuScreenPanel.SetActive(false);
+
+        optionsScreenPanel.SetActive(false);
+
+        pauseMenuScreenPanel.SetActive(false);
+
+        endScreenPanel.SetActive(true);
+    }
+
+    public void VolumeValue()
+    {
+        volume.wholeNumbers = true;
+        volumeValue.text = $"{volume.value}%";
+    }
+    public void PlayGame()
+    {
+        mainMenuScreenPanel.SetActive(false);
+        endScreenPanel.SetActive(false);
+        pauseMenuScreenPanel.SetActive(false);
+
+    }
+    private void PauseGame()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            pauseMenuScreenPanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenuScreenPanel.SetActive(false);
+        }
+
+        mainMenuScreenPanel.SetActive(false);
+        endScreenPanel.SetActive(false);
+    }
+
+    private void LooseScenario()
+    {
+        titleScreenPanel.SetActive(false);
+
+        mainMenuScreenPanel.SetActive(true);
+
+        optionsScreenPanel.SetActive(false);
+
+        pauseMenuScreenPanel.SetActive(false);
+
+        endScreenPanel.SetActive(true);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log($"Application has quit");
+    }
+
+    public void HasPressedKeyDetection()
+    {
+        hasPressedAnyKey = false;
+        TitleScreen();
+    }
+
+}
