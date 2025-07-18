e1cfbe0f5089f3479ad970f3389d2678a3760d6d
BearM1n1mum <bob.gaming.cro@gmail.com>
2025-07-18 18:00:57 +0200

    SaveSystem

---
+++ Survival Clicker/Assets/_Scripts/SaveSystem.cs
@@ -0,0 +1,27 @@
+//using System;
+//using UnityEngine;
+//using UnityEngine.Rendering.VirtualTexturing;
+
+//[Serializable]
+//public static class SaveSystem
+//{
+
+////napravite save system skriptu koja NIJE monobehavour u kojoj se moze spremiti i dohvatiti float, int i string
+////BONUS*:
+////U igri Clicker, napravite da se spremi koliko dugo ste prezivjeli igrajuci igru
+////Bonus2:
+////U clicker igri uz bonus jedan napravite da si spremite i ime
+////*Bonus vam donosi jedan plus
+////Bonus2 donosi 2 plusa
+
+//    private const string HIGHSCORE_KEY = "highScore";
+
+//    public static void SaveHighScore(int score)
+//    {
+//        PlayerPrefs.SetInt(HIGHSCORE_KEY, score);
+//        PlayerPrefs.Save();
+//    }
+
+
+
+//}
